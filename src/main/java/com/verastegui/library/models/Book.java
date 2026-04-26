6e14640406366769d3f931cb6f2eda5a4c53f61d
darwin <darwinjverasteguis2002@gmail.com>
2026-04-26 11:32:16 -0500

    Add Getter and Setter for Book class

--- src/main/java/com/verastegui/library/models/Book.java
+++ src/main/java/com/verastegui/library/models/Book.java
@@ -1,3 +1,34 @@
 public class Book {
+    private String name;
+    private String author;
+    private float price;
+
+    public void setName(String name) {
+        name = this.name;
+    }
     
+    public void setAuthor(String author) {
+        author = this.author;
+    }
+
+    public void setPrice(float price) {
+        price = this.price;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public float getPrice() {
+        return this.price;
+    }
+    
+    @Override
+    public String toString() {
+        return "Book name: " + this.name + " - Author: " + this.author + " - price -> " + this.price;
+    }
 }
