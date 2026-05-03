71d7b31edb08c22e1415b542cbc2c6eb84c62120
darwin <darwinjverasteguis2002@gmail.com>
2026-05-03 15:39:43 -0500

    Add console for user interaction

--- src/main/java/com/verastegui/library/Main.java
+++ src/main/java/com/verastegui/library/Main.java
@@ -6,15 +6,53 @@ import com.verastegui.library.models.Book;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Book book1 = new Book();
+        Book[] books = new Book[10];
+        Book book = new Book();
 
-        System.err.println("Enter the book name: ");
-        book1.setName(sc.nextLine());
-        System.err.println("Enter the book author: ");
-        book1.setAuthor(sc.nextLine());
-        System.err.println("Enter the book price: ");
-        book1.setPrice(sc.nextFloat());
+        while (true) { 
+            System.out.println("Plase choose an option: \n1. Add book \n2. Show books \n3. Exit");
+            String userAction = sc.nextLine();
 
-        System.out.println(book1.toString());
+            switch (userAction) {
+            
+                case "1":
+                    System.out.println("Enter the book name: ");
+                    String name = sc.nextLine();
+                    book.setName(name);
+
+                    System.out.println("Enter the book author: ");
+                    String author = sc.nextLine();
+                    book.setAuthor(author);
+
+                    System.out.println("Enter the book price: ");
+                    float price = sc.nextFloat();
+                    sc.nextLine();
+                    book.setPrice(price);
+
+                    for (int i = 0; i < books.length; i++) {
+                        if (books[i] == null) {
+                            books[i] = book;
+                            break;
+                        }
+                    }
+                    
+                    break;
+                case "2":
+                    for (int i = 0; i < books.length; i++) {
+                        if (books[i] != null) {
+                            System.out.println(books[i].toString());
+                        }
+
+                    }
+                    
+                    break;
+                case "3":
+                    sc.close();
+                    System.exit(0);
+                default:
+                    System.err.println("Invalid option, please try again.");
+            }
+            
+        }
     }
 }
