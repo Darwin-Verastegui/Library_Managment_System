0b7e15a976e1d331677816485a95e4eedfd5ab03
darwin <darwinjverasteguis2002@gmail.com>
2026-05-17 11:17:09 -0500

    Implement DigitalBook model

---
+++ src/main/java/com/verastegui/library/models/DigitalBook.java
@@ -0,0 +1,17 @@
+public class DigitalBook extends Book {
+    private String format;
+
+    public DigitalBook(String name, String author, float price, String format) {
+        super(name, author, price);
+        this.format = format;
+    }
+
+    public String getFormat() {
+        return this.format;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " - Format: " + this.format;
+    }
+}
